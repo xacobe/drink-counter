86323eceb16b0e4b688d6a731f924b0a9dd42ea8
xacobe <xacobe.net@gmail.com>
2023-04-29 17:13:36 +0200

    initial commit
    First commit with basic functionality.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Key stroke velocity counter
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/xacobe/pen/eYPEvrE](https://codepen.io/xacobe/pen/eYPEvrE).
+
---
+++ script.js
@@ -0,0 +1,141 @@
+const drinkSelector = document.getElementById("drinkSelector");
+const addDrinkBtn = document.getElementById("addDrinkBtn");
+const subtractDrinkBtn = document.getElementById("subtractDrinkBtn");
+const addDrinkForm = document.getElementById("addDrinkForm");
+const resetBtn = document.getElementById("resetBtn");
+const drinkCount = document.getElementById("drinkCount");
+const alcoholCount = document.getElementById("alcoholCount");
+const drinkTable = document.getElementById("drinkTable");
+
+let drinks = JSON.parse(localStorage.getItem("drinks")) || [];
+let drinkHistory = JSON.parse(localStorage.getItem("drinkHistory")) || [];
+
+function populateDrinkSelector() {
+  drinks.forEach((drink) => {
+    const option = document.createElement("option");
+    option.value = JSON.stringify(drink);
+    option.textContent = `${drink.name} - ${drink.brand} (${drink.alcohol}%)`;
+    drinkSelector.appendChild(option);
+  });
+}
+
+function addDrink() {
+  // Función para añadir una bebida al historial
+  const selectedDrink = JSON.parse(drinkSelector.value);
+    if (!selectedDrink) return;
+
+    const drinkEntry = {
+        ...selectedDrink,
+        timestamp: new Date().getTime()
+    };
+
+    drinkHistory.push(drinkEntry);
+    localStorage.setItem("drinkHistory", JSON.stringify(drinkHistory));
+
+    updateDisplay();
+}
+
+function subtractDrink() {
+  // Función para restar una bebida del historial
+  if (drinkHistory.length === 0) return;
+
+    drinkHistory.pop();
+    localStorage.setItem("drinkHistory", JSON.stringify(drinkHistory));
+
+    updateDisplay();
+}
+
+function addNewDrink(e) {
+    e.preventDefault();
+
+    const newDrink = {
+        name: addDrinkForm.name.value,
+        brand: addDrinkForm.brand.value,
+        alcohol: parseFloat(addDrinkForm.alcohol.value),
+        isInvited: addDrinkForm.isInvited.checked
+    };
+
+    drinks.push(newDrink);
+    localStorage.setItem("drinks", JSON.stringify(drinks));
+
+    // Limpiar formulario
+    addDrinkForm.reset();
+
+    // Actualizar el selector de bebidas y la vista
+    populateDrinkSelector();
+    updateDisplay();
+}
+
+// Función para resetear el contador de bebidas en las últimas 12 horas
+function resetDrinkCounter() {
+  const currentTime = new Date().getTime();
+  const twelveHoursAgo = currentTime - 12 * 60 * 60 * 1000;
+
+  drinkHistory = drinkHistory.filter(
+    (entry) => entry.timestamp >= twelveHoursAgo
+  );
+  localStorage.setItem("drinkHistory", JSON.stringify(drinkHistory));
+
+  updateDisplay();
+}
+
+// Función para actualizar la vista
+function updateDisplay() {
+  // Actualizar contador de bebidas y alcohol en las últimas 12 horas
+  const currentTime = new Date().getTime();
+  const twelveHoursAgo = currentTime - 12 * 60 * 60 * 1000;
+  const last12Hours = drinkHistory.filter(
+    (entry) => entry.timestamp >= twelveHoursAgo
+  );
+
+  drinkCount.textContent = last12Hours.length;
+  alcoholCount.textContent = last12Hours
+    .reduce((total, entry) => total + entry.alcohol, 0)
+    .toFixed(2);
+
+  // Actualizar tabla
+  const tbody = drinkTable.querySelector("tbody");
+  tbody.innerHTML = "";
+
+  drinkHistory.forEach((entry, index) => {
+    const row = document.createElement("tr");
+
+    const nameCell = document.createElement("td");
+    nameCell.textContent = entry.name;
+    row.appendChild(nameCell);
+
+    const alcoholCell = document.createElement("td");
+    alcoholCell.textContent = entry.alcohol;
+    row.appendChild(alcoholCell);
+
+    const date = new Date(entry.timestamp);
+    const dateCell = document.createElement("td");
+    dateCell.textContent = `${date.getHours()}:${date.getMinutes()}-${
+      date.getMonth() + 1
+    }-${date.getFullYear()}`;
+    row.appendChild(dateCell);
+
+    const timeDifference =
+      index === 0 ? 0 : entry.timestamp - drinkHistory[index - 1].timestamp;
+    const timeDifferenceCell = document.createElement("td");
+    const hours = Math.floor(timeDifference / (60 * 60 * 1000));
+    const minutes = Math.floor(
+      (timeDifference % (60 * 60 * 1000)) / (60 * 1000)
+    );
+    const seconds = Math.floor((timeDifference % (60 * 1000)) / 1000);
+    timeDifferenceCell.textContent = `${hours}:${minutes}:${seconds}`;
+    row.appendChild(timeDifferenceCell);
+
+    tbody.appendChild(row);
+  });
+}
+
+// Event listeners
+addDrinkBtn.addEventListener("click", addDrink);
+subtractDrinkBtn.addEventListener("click", subtractDrink);
+addDrinkForm.addEventListener("submit", addNewDrink);
+resetBtn.addEventListener("click", resetDrinkCounter);
+
+// Inicialización
+populateDrinkSelector();
+updateDisplay();
