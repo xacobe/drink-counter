8ad6bee2dab25bfd538b24f8a984d27548f02e48
xacobe <xacobe.net@gmail.com>
2023-05-01 11:28:15 +0200

    Revert "remove invitada state"
    This reverts commit fd400ce5cf199b2705f454f859ccf4aa01228a22.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const resetBtn = document.getElementById("resetBtn");
 const addDrinkForm = document.getElementById("addDrinkForm");
 const addBarForm = document.getElementById("addBarForm");
 const barSelector = document.getElementById("barSelector");
+const isInvitedCheckbox = document.getElementById("isInvitedCheckbox");
 const isPaidCheckbox = document.getElementById("isPaidCheckbox");
 const drinkCount = document.getElementById("drinkCount");
 const alcoholCount = document.getElementById("alcoholCount");
@@ -160,6 +161,17 @@ function updateDisplay() {
     paidCell.textContent = entry.isPaid ? "Sí" : "No";
     row.appendChild(paidCell);
 
+    const invitedCell = document.createElement("td");
+    invitedCell.className = "invited";
+    invitedCell.textContent = entry.isInvited ? "Sí" : "No";
+    row.appendChild(invitedCell);
+
+    // Crea un input tipo checkbox para la celda de invitaciones
+    const invitedCheckbox = document.createElement("input");
+    invitedCheckbox.type = "checkbox";
+    invitedCheckbox.checked = entry.isInvited;
+    invitedCell.appendChild(invitedCheckbox);
+
     // Crea un input tipo checkbox para la celda de pagos
     const paidCheckbox = document.createElement("input");
     paidCheckbox.type = "checkbox";
@@ -177,6 +189,12 @@ function updateDisplay() {
       drinkHistory.splice(index, 1);
       localStorage.setItem("drinkHistory", JSON.stringify(drinkHistory));
       updateDisplay();
+    });
+
+    // Añade eventos de cambio a los checkboxes
+    invitedCheckbox.addEventListener("change", () => {
+      entry.isInvited = !entry.isInvited;
+      localStorage.setItem("drinkHistory", JSON.stringify(drinkHistory));
     });
 
     paidCheckbox.addEventListener("change", () => {
